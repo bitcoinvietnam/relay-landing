a8318862b8ccc54f9ecf674492724c9f54066164
rowenslee <rowens.lee@gmail.com>
2024-10-02 10:09:36 +0700

    Add base url config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'DEV' ? '' : '/relay-landing/',
   plugins: [vue()],
   resolve: {
     alias: {
