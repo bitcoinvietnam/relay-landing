17370aa8cb91fcb03aab61c26f61f8adf4a5f49c
rowenslee <rowens.lee@gmail.com>
2024-10-02 10:32:53 +0700

    Fix base url for staging env

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'DEV' ? '' : '/relay-landing/',
+  base: process.env.NODE_ENV === 'STAGE' ? '/relay-landing/' : '',
   plugins: [vue()],
   resolve: {
     alias: {
