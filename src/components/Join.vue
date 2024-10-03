6152ecea6f3f5024edcb8991ea60142cf1ada9a8
rowenslee <rowens.lee@gmail.com>
2024-10-03 17:13:12 +0700

    Change some words

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,9 +1,8 @@
 <template>
-
   <img src="@/assets/blue-spark.webp" class="position-absolute top-50 start-50 translate-middle" style="z-index: -1;">
   <div class="card bg-transparent col col-md-10 col-lg-6 ">
     <div class="card-body">
-      <h2>The First Ever Nostr Relay In Vietnam</h2>
+      <h2>Premium Nostr Relay In Vietnam</h2>
       <a href="https://nostr.bitcoinvn.io/invoices">
         <button type="submit" class="btn btn-primary btn-lg px-3">
           Join BV Relay
