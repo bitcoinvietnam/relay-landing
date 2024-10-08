62afaf5bb94f41098d7cfa802795ff3f20ac817f
rowenslee <rowens.lee@gmail.com>
2024-10-08 10:34:22 +0700

    Fix a word

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -2,7 +2,7 @@
   <img src="@/assets/blue-spark.webp" class="position-absolute top-50 start-50 translate-middle" style="z-index: -1;">
   <div class="card bg-transparent col col-md-10 col-lg-6 ">
     <div class="card-body">
-      <h2>Premium Nostr Relay In Vietnam</h2>
+      <h2>Premier Nostr Relay In Vietnam</h2>
       <a href="https://nostr.bitcoinvn.io/invoices">
         <button type="submit" class="btn btn-primary btn-lg px-3">
           Join BV Relay
